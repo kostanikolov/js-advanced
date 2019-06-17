5ebd0520400d8eeaa963e2eac55b2f34ad214e8b
kostanikolov <kosta94@mail.bg>
2019-06-17 12:52:38 +0300

    adding files

--- 01-AdvancedFunctions/lab/aggregates.js
+++ 01-AdvancedFunctions/lab/aggregates.js
@@ -1,32 +1,25 @@
 function aggregates(array) {
     
-    const sum = () => {
-        return array.reduce((sum, acc) => sum += acc);
-    }
+    console.log('Sum =', reducer(array, (a, b) => a + b, 0));
+    console.log('Min =', reducer(array, (a, b) => a < b ? a : b, Number.MAX_SAFE_INTEGER));
+    console.log('Max =', reducer(array, (a, b) => a > b ? a : b, Number.MIN_SAFE_INTEGER));
+    console.log('Product =', reducer(array, (a, b) => a * b, 1));
+    console.log('Join =', reducer(array, (a, b) => a + b, ''));
 
-    const min = () => {
-        return Math.min(...array);
+    function reducer(arr, operator, initial) {
+        let result = initial;
+    
+        for (let element of arr) {
+            result = operator(result, element);
+        }
+    
+        return result;
     }
-
-    const max = () => {
-        return Math.max(...array);
-    }
-
-    const product = () => {
-        return array.reduce((sum, acc) => sum *= acc);
-    }
-
-    const join = () => {
-        return array.reduce((sum, acc) => sum += acc, '');
-    }
-
-    console.log(sum());
-    console.log(min());
-    console.log(max());
-    console.log(product());
-    console.log(join());
 }
 
-aggregates([2, 3, 10, 5]);
+const firstInput = [2, 3, 10, 5];
+const secondInput = [5, -3, 20, 7, 0.5];
+
+aggregates(firstInput);
 console.log('=============');
-aggregates([5, -3, 20, 7, 0.5]);
+aggregates(secondInput);
