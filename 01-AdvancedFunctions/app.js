7b1599e1864197db3eeddeed54afe28a78685d60
kostanikolov <kosta94@mail.bg>
2019-06-19 11:41:19 +0300

    adding files

--- 01-AdvancedFunctions/app.js
+++ 01-AdvancedFunctions/app.js
@@ -1,5 +1,23 @@
-function solve() {
+function solve(text) {
 
+    let alphabet = {};
+
+    for (let letter = 0; letter < text.length; letter++) {
+        let currentLetter = text[letter];
+
+        if (!alphabet.hasOwnProperty(currentLetter)) {
+            alphabet[currentLetter] = 1;
+        } else {
+            alphabet[currentLetter]++;
+        }
+    }
+
+    Object.keys(alphabet)
+        .sort((a, b) => alphabet[b] - alphabet[a])
+        .forEach(key => console.log(`${key} = ${alphabet[key]}`));
 }
 
-solve();
+// solve('the quick brown fox jumps over the lazy dog');
+
+console.log(`the quick brown fox jumps over the lazy dog`);
+console.log(`THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG`);
---
+++ 01-AdvancedFunctions/exercise/functionalSum.js
@@ -0,0 +1,53 @@
+const fn = (function () {
+    let sum = 0;
+
+    return function add(number) {
+        sum += number;
+
+        add.toString = function () {
+            return sum;
+        };
+
+        return add;
+    }
+})();
+
+console.log(fn(3)(5)(8).toString());
+
+
+
+
+
+
+
+
+
+// function add(num) {
+//     let sum = num;
+
+//     function calc(num2) {
+//         sum += num2;
+//         return calc;
+//     }
+
+//     calc.toString = function () {
+//         return sum;
+//     };
+
+//     return calc;
+// }
+
+// console.log(add(1)(6)(-3).toString());
+
+// let f = (function () {
+//     let total = 0;
+
+//     return function sum(a) {
+//         total += a;
+//         sum.toString = () => { return total; };
+
+//         return sum;
+//     }
+// })();
+
+// console.log(f(6)(7).toString());
