4c992120fc7e9822233f4c0ca34957edcc310972
kostanikolov <kosta94@mail.bg>
2019-06-21 15:23:38 +0300

    modified

--- 02-DOMManipulations/app.js
+++ 02-DOMManipulations/app.js
@@ -1,9 +1,5 @@
 function app() {
-
-    const inputField = document.getElementsByTagName('input')[0];
-    const button = document.getElementsByTagName('button')[1];
-
-    inputField.addEventListener('input', function () {
-        button.setAttribute('disabled', 'false');
-    });
+    console.log('Hello World!');
 }
+
+app();
