0ef6bbc36e2bdba6a0d76c6e3b2e5b547cd54579
kostanikolov <kosta94@mail.bg>
2019-06-21 10:35:58 +0300

    modified file

--- 02-DOMManipulations/lab/03. Delete-From-Table/app.js
+++ 02-DOMManipulations/lab/03. Delete-From-Table/app.js
@@ -1,3 +1,30 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+
+    const SELECTORS = {
+        TABLE_ROWS: '#customers tr td:last-child',
+        MESSAGE: 'result',
+        EMAIL: 'email'
+    }
+
+    const message = document.getElementById(SELECTORS.MESSAGE);
+    deleteEmail();
+
+    function getEmail() {
+        return document.getElementsByName(SELECTORS.EMAIL)[0].value;
+    }
+
+    function deleteEmail() {
+        const rows = document.querySelectorAll(SELECTORS.TABLE_ROWS);
+        const email = getEmail();
+        const rowToDelete = Array.from(rows)
+            .find(row => row.textContent === email);
+
+        if (rowToDelete) {
+            let row = rowToDelete.parentNode;
+            row.parentNode.removeChild(row);
+            message.textContent = 'Deleted.';
+        } else {
+            message.textContent = 'Not found.';
+        }
+    }
 }
