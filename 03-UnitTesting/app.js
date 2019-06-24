82bb89962363bb1fb57f1391396d34567d5402a8
kostanikolov <kosta94@mail.bg>
2019-06-24 14:34:33 +0300

    modified

---
+++ 03-UnitTesting/app.js
@@ -0,0 +1,4 @@
+let date = new Date(2016, 1, 31);
+console.log(date);
+
+parseInt()
---
+++ 03-UnitTesting/main.ts
@@ -0,0 +1,7 @@
+function add(n1: number, n2: number): number {
+
+    return n1 + n2;
+}
+
+var x = add(5, 3);
+console.log(x);
