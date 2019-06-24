d1b90f11bcfb594d25d3e1bd9d8a77474ee366e9
kostanikolov <kosta94@mail.bg>
2019-06-24 21:17:17 +0300

    modified

--- 03-UnitTesting/app.js
+++ 03-UnitTesting/app.js
@@ -1,4 +1,53 @@
-let date = new Date(2016, 1, 31);
-console.log(date);
+const codeNames = [
+    'Alpha',
+    'Bravo',
+    'Charlie',
+    'Delta',
+    'Echo',
+    'Foxtrot',
+    'Golf',
+    'Hotel',
+    'India',
+    'Juliett',
+    'Kilo',
+    'Lima',
+    'Mike',
+    'November',
+    'Oscar',
+    'Papa',
+    'Quebec',
+    'Romeo',
+    'Sierra',
+    'Tango',
+    'Uniform',
+    'Victor',
+    'Whiskey',
+    'X-ray',
+    'Yankee',
+    'Zulu'
+];
 
-parseInt()
+class Dog {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    static getRandomAge() {
+        return Math.ceil(Math.random() * 12);
+    }
+
+    toString() {
+        return `My name is ${this.name} and I am ${this.age}-years-old.`;
+    }
+}
+
+let dogs = [];
+
+for (const name of codeNames) {
+    const dog = new Dog(name, Dog.getRandomAge());
+    dogs.push(dog);
+}
+
+dogs.sort((a, b) => b.age - a.age)
+    .forEach(dog => console.log(dog.toString()));
