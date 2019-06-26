2d007343e97fec12c5c59633697f6f03bad5f49d
kostanikolov <kosta94@mail.bg>
2019-06-26 17:35:14 +0300

    modified

--- 03-UnitTesting/app.js
+++ 03-UnitTesting/app.js
@@ -1,53 +1,9 @@
-const codeNames = [
-    'Alpha',
-    'Bravo',
-    'Charlie',
-    'Delta',
-    'Echo',
-    'Foxtrot',
-    'Golf',
-    'Hotel',
-    'India',
-    'Juliett',
-    'Kilo',
-    'Lima',
-    'Mike',
-    'November',
-    'Oscar',
-    'Papa',
-    'Quebec',
-    'Romeo',
-    'Sierra',
-    'Tango',
-    'Uniform',
-    'Victor',
-    'Whiskey',
-    'X-ray',
-    'Yankee',
-    'Zulu'
-];
-
-class Dog {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    static getRandomAge() {
-        return Math.ceil(Math.random() * 12);
-    }
-
-    toString() {
-        return `My name is ${this.name} and I am ${this.age}-years-old.`;
-    }
+const input0 = {
+    method: 'GET',
+    uri: 'svn.public.catalog',
+    version: 'HTTP/1.1',
+    message: ''
 }
 
-let dogs = [];
-
-for (const name of codeNames) {
-    const dog = new Dog(name, Dog.getRandomAge());
-    dogs.push(dog);
-}
-
-dogs.sort((a, b) => b.age - a.age)
-    .forEach(dog => console.log(dog.toString()));
+const props = ['method', 'uri', 'version', 'message'];
+const inputProps = Object.keys(input0);
--- 03-UnitTesting/main.ts
+++
@@ -1,7 +0,0 @@
-function add(n1: number, n2: number): number {
-
-    return n1 + n2;
-}
-
-var x = add(5, 3);
-console.log(x);
